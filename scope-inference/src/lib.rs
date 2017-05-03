df9683341d860c94cbc4c6cc6f403bb15b6ba4cd
Justin <zallambo@gmail.com>
2017-05-02 23:09:51 -0400

    Add test todo

--- scope-inference/src/lib.rs
+++ scope-inference/src/lib.rs
@@ -1,3 +1,9 @@
+/* [TODO]:
+ *   Test case: error messages from the two Pi calculus rules:
+ *   P | (v x) Q    =>  (v x) (P | Q)
+ *   (v x) (P | Q)  =>  P | (v x) Q
+ */
+
 extern crate regex;
 #[macro_use] extern crate lazy_static;
 
