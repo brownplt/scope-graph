a7effa81872da960928dd9f8487782c234961f65
Justin <zallambo@gmail.com>
2017-05-15 13:59:47 -0400

    chore: Push zip-file-making script

---
+++ scope-inference/make_zip.sh
@@ -0,0 +1,3 @@
+cd ..
+zip scope-inference.zip scope-inference/README.md scope-inference/Cargo.toml scope-inference/src/* scope-inference/src/parser/* scope-inference/src/tests/* scope-inference/src/examples/*
+mv scope-inference.zip scope-inference/
