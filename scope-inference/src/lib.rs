4b7c2856ada0648a65bfcf799c9e386e9a98264f
Justin <justinpombrio@cs.brown.edu>
2017-05-15 12:12:29 -0400

    chore: Improve readme

--- scope-inference/src/lib.rs
+++ scope-inference/src/lib.rs
@@ -1,24 +1,25 @@
 //! # Scope Inference
-//!
+//! 
 //! This is the reference implementation for "Inferring Scope through Syntactic Sugar", ICFP'17.
-
+//! 
 //! ## Artifact Evaluation Instructions
 //! 
 //! #### Step 1: Install Rust
-//!
+//! 
 //! Follow the the `Installation` instructions below.
-//!
+//! 
 //! #### Step 2: Run the tests
-//!
+//! 
 //! In the scope-inference directory, run `cargo test`.
+//! They should all pass.
 //! (This runs `src/test.rs`.)
-//!
+//! 
 //! This runs the test cases, but does not show the inferred scopes.
 //! To see the inferred scopes and total inference time,
 //! run `cargo run timing_test`. (This runs `timing_test()` in `src/main.rs`.)
-//!
+//! 
 //! Both of these commands infer scope for five languages:
-//!
+//! 
 //! - Single-arm let (example 1 from the paper)
 //! - Multi-arm let (example 2 from the paper)
 //! - Haskell List Comprehensions (section 6: evaluation)
@@ -26,7 +27,8 @@
 //! - R5RS sugars that bind values (section 6: evaluation)
 //! 
 //! Here are the claims from the paper that can be verified against
-//! this implementation.
+//! this implementation. To make sense of these, you may want to
+//! read the `Overview` below, or see section 4.3 of the paper.
 //! 
 //! - Section 2.1: single-arm let is the first test language.
 //!   Run `cargo run src/examples/single_arm_let.scope`.
@@ -42,24 +44,24 @@
 //!   and `cargo run src/examples/pyret.scope`.
 //! - Section 6.4: you can see scope inference fail on the `do` sugar
 //! by running `cargo run src/examples/do.scope`.
-
+//! 
 //! ## Installation
-//!
+//! 
 //! To install Rust, run:
 //! ```curl -sSf https://static.rust-lang.org/rustup.sh | sh```
-//!
+//! 
 //! The installation script may suggest that you update your PATH
 //! environment variable; do as suggested.
-//!
+//! 
 //! If you have any troubles, or wish to install Rust using a
 //! different method, see the official
 //! [Rust installation instructions](https://www.rust-lang.org/en-US/install.html).
-//!
+//! 
 //! This has been tested on Rust 1.9 and 1.17, though later versions
 //! should work as well.
 //! 
 //! ## Usage
-//!
+//! 
 //! Input files (extension `.scope`) contain sugar definitions and
 //! core language scoping rules. Scope inference then determines the
 //! minimal set of scoping rules for the sugars such that desugaring
@@ -67,13 +69,13 @@
 //! 
 //! To infer scope for an input file:
 //! ```cargo run filename.scope```
-//!
+//! 
 //! To read a .scope file through stdin:
 //! ```cargo run stdin```
-//!
+//! 
 //! Examples of language definitions (mainly from the paper) may be
 //! found in `src/examples/`.
-
+//! 
 //! ## Overview
 //! 
 //! The language definition files end in ".scope". Examples can be found in `src/examples/`.
@@ -85,8 +87,11 @@
 //! 
 //! - `import i`: Provide lexical scope to child i.
 //!               (You almost certainly want 'import i' for each 'i'.)
+//!               In the diagrams in the paper, this is a *down* arrow.
 //! - `export i`: Export child i's declarations.
+//!               In diagrams, this is an *up* arrow.
 //! - `bind i in j`: Make child i's declarations available in child j.
+//!               In diagrams, this is an *across* arrow.
 //! - `re-export`: Export your imports.
 //!                (This is rarely if ever needed, but provided for completion.)
 //! 
@@ -109,7 +114,7 @@
 //!   rule (Let a b c) => (Apply (Lambda a c) b)
 //! }
 //! ```
-
+//! 
 //! ## Extra Features
 //! 
 //! The implementation has one feature mentioned but not discussed in the paper: when
@@ -122,7 +127,7 @@
 //!         disjoint bind var in binds;
 //!       }
 //! ```
-
+//! 
 //! ## Files
 //! 
 //!  - `src/parser/*`:       parser & lexer
